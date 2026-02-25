fe55c0823d37a0cda93389e4ca321036612c6dcc
Bharath-saladi <bharathsaladi1@gmail.com>
2026-02-25 10:31:20 +0530

    UC2:Hardcoded palindrome

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,7 +1,18 @@
 public class UseCasePalindromeCheckerApp {
     public static void main (String[] args) {
-        System.out.println("welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initislized successfully");
+            String input = "madam";
+            boolean isPalindrome = true;
+            for (int i = 0; i < input.length() / 2; i++) {
+                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome) {
+                System.out.println(input + " is a Palindrome");
+            } else {
+                System.out.println(input + " is not a Palindrome");
+            }
+        }
     }
-}
+
