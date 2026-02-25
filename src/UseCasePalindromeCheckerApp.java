ae27c89c75c691cb70b740fb03918fa7f2baba6c
Bharath-saladi <bharathsaladi1@gmail.com>
2026-02-25 10:42:29 +0530

    UC3

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,18 +1,18 @@
 public class UseCasePalindromeCheckerApp {
     public static void main (String[] args) {
-            String input = "madam";
-            boolean isPalindrome = true;
-            for (int i = 0; i < input.length() / 2; i++) {
-                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-            if (isPalindrome) {
-                System.out.println(input + " is a Palindrome");
-            } else {
-                System.out.println(input + " is not a Palindrome");
-            }
+        String input = "madam";
+        String reversed = "";
+
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
         }
     }
 
