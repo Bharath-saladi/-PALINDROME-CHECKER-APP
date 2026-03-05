d87e593309df96fd27a75969fbbfcf9421839191
Bharath-saladi <bharathsaladi1@gmail.com>
2026-03-05 09:59:34 +0530

    UC11:Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,23 +5,27 @@ public class PalindromeCheckerApp {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input: ");
+        System.out.print("Input : ");
         String input = scanner.nextLine();
 
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        boolean result = check(input, 0, input.length() - 1);
 
-        boolean isPalindrome = true;
-
-        for (int i = 0; i < normalized.length() / 2; i++) {
-
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Is Palindrome?: " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
 
         scanner.close();
+    }
+
+    private static boolean check(String s, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        return check(s, start + 1, end - 1);
 
     }
 }
